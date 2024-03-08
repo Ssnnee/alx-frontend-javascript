bcff89080efcbe313b0d0ad07e3860fb6abe531e
Ssnnee <nandisamuelsne@gmail.com>
2024-03-08 19:55:15 +0100

    feat: prevete divison by 0

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,13 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = (firstName, lastName, fileName) =>
-  Promise.allSettled(
-    [
-      signUpUser(firstName, lastName), uploadPhoto(fileName)
-    ])
-    .then((results) =>
-      results.map((result) =>
-        result.status === 'fulfilled'
-        ? { status: result.status, value: result.value }
-        : { status: result.status, value: `Error: ${result.reason.message}` }
-      )
-    );
+const handleProfileSignup = (firstName, lastName, fileName) => Promise.allSettled(
+  [
+    signUpUser(firstName, lastName), uploadPhoto(fileName),
+  ],
+)
+  .then((results) => results.map((result) => (result.status === 'fulfilled'
+    ? { status: result.status, value: result.value }
+    : { status: result.status, value: `Error: ${result.reason.message}` })));
 
 export default handleProfileSignup;
