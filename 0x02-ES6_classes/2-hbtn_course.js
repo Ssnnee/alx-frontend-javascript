15c9218d90d17b658ac7d451bbd6090b3957c0e1
Samuel Nandi <nandisamuelsne@gmail.com>
2024-06-29 14:21:01 +0100

    fix: make the check in the constrator

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,20 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = name;
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = length;
+
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    } else if (students.some((student) => typeof student !== 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._students = students;
   }
 
